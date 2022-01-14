2a69f3e6ff25e6225b2259e63f6bffbe6420bdb8
diego boy <diegolive5000@gmail.com>
2022-01-14 18:36:47 +0100

    remove image from products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_151829) do
+ActiveRecord::Schema.define(version: 2022_01_14_173154) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.string "image"
     t.string "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

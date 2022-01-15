0fdd18059317658b0f2d3b2464498c598c78d12a
diego boy <diegolive5000@gmail.com>
2022-01-15 15:21:31 +0100

    add active storage, create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "❌ destroying products"
+
+Product.destroy_all
+
+puts "✅ products destroyed successfully"
+puts "..."
+puts "🌱 creating products"
+
+Product.create(title: "Snowboard", description: "Glides like butter", image: "./../app/assets/images/snowboard-unsplash.jpg", price: "120")
+
+Product.create(title: "Nintendo Switch", description: "Play fun games everywhere", image: "./../app/assets/images/nintendoswitch-unsplash.jpg", price: "349,99")
+
+puts "✅ products created successfully"
